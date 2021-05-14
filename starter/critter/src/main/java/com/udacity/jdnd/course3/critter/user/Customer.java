30c787552dfdb669e41a9d1372454146c95fddf5
Emerson Orci <erorci@gmail.com>
2021-05-14 11:30:36 +0200

    Configure properties and design Entities To Represent Data

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,49 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import org.assertj.core.util.Lists;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Customer extends User {
+    @Column(name = "phoneNumber_full", length = 15)
+    private String phoneNumber;
+
+    @Nationalized
+    private String notes;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Pet> pets;
+
+    public Customer() {
+        pets = Lists.emptyList();
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+        pet.setOwner(this);
+    }
+
+    public List<Pet> getPets() {
+        return Lists.newArrayList(pets);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
